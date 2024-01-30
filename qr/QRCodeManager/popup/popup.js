f0588cf9b82a240700536fd70bc34e2b38ab0032
kreker990 <akparovbeka@gmail.com>
2024-01-30 11:07:28 +0600

    add qr

--- qr/QRCodeManager/popup/popup.js
+++ qr/QRCodeManager/popup/popup.js
@@ -1,4 +1,4 @@
-const file = document.querySelector('#file')
+// const file = document.querySelector('#file')
 
 function fetchRequest(formData){
     fetch('http://api.qrserver.com/v1/read-qr-code/',{
@@ -8,9 +8,29 @@ function fetchRequest(formData){
     })
 } 
 
-file.addEventListener('change', e=>{
-    let file = e.target.files[0]
-    let formData = new FormData();
-    formData.append('file', file)
-    fetchRequest(formData)
-})
+// file.addEventListener('change', e=>{
+//     let file = e.target.files[0]
+//     let formData = new FormData();
+//     formData.append('file', file)
+//     fetchRequest(formData)
+// })
+const btn = document.querySelector('#runFunctionButton')
+btn.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "reloadFunction"});
+    });
+});
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.formData) {
+        console.log(4)
+        const receivedData = message.formData;
+        const formData = new FormData();
+
+        for (const key in receivedData) {
+            formData.append(key, receivedData[key]);
+        }
+        fetchRequest(formData)
+    }
+});
